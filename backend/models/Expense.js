5ff306501c503ecbbee3f5302e42bd2d9bd707f2
Krishna Shukla <backend.9developer@gmail.com>
2025-09-13 00:17:14 +0530

    test

--- backend/models/Expense.js
+++ backend/models/Expense.js
@@ -2,9 +2,13 @@ const mongoose = require('mongoose');
 
 const expenseSchema = new mongoose.Schema({
   title: { type: String, required: true },
-    type: { type: String, required: true },  // <-- ADD THIS
+    // type: { type: String, required: true },  // <-- ADD THIS
+    type: { type: String, default: 'Expense' },
+
   amount: { type: Number, required: true },
-  category: { type: String }, // e.g., "Salary", "Stationary", "Maintenance"
+  // category: { type: String }, // e.g., "Salary", "Stationary", "Maintenance"
+  category: { type: String, enum: ['Salaries','Incentives','Rent','Travel Allowance Agent','Travel Allowance Owner','Meta Recharge','Chargeback','Refunds','Miscellaneous Expenses','Call Payment'] },
+
   date: { type: Date, default: Date.now },
   notes: { type: String },
   user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }, // Added user field
--- backend/models/Income.js
+++ backend/models/Income.js
@@ -2,9 +2,13 @@ const mongoose = require('mongoose');
 
 const incomeSchema = new mongoose.Schema({
   title: { type: String, required: true },
-   type: { type: String, required: true },  // <-- ADD THIS
+  //  type: { type: String, required: true },  // <-- ADD THIS
+   type: { type: String, default: 'Income' },
+
   amount: { type: Number, required: true },
-  category: { type: String }, // optional, e.g., "Donation", "Other"
+  // category: { type: String }, // optional, e.g., "Donation", "Other"
+  category: { type: String, enum: ['MCO Meta','MCO PPC','Meta Rental','Commission','Technology Sale','Domestic Tour Package','International Tour Package','Airline Ticket','Hotel','Car Hire','Activities','Airport Transfers','Visa'] },
+
   date: { type: Date, default: Date.now },
   notes: { type: String },
   user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }, // Added user field
