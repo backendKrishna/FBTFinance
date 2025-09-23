d19a86f6056f84c09317a36e8a0a11f2b300ac47
Krishna Shukla <backend.9developer@gmail.com>
2025-09-23 22:15:27 +0530

    test

--- backend/routes/financeRoutes.js
+++ backend/routes/financeRoutes.js
@@ -11,17 +11,23 @@ const router = express.Router();
 
 // only admins may use these endpoints
 router.post('/income', protect, adminOnly, addIncome);
-router.get('/incomes', protect, adminOnly, getIncomes);
+// router.get('/incomes', protect, adminOnly, getIncomes);
+router.get('/incomes', protect, getIncomes);
+
 router.put('/income/:id', protect, adminOnly, updateIncome);
 router.delete('/income/:id', protect, adminOnly, deleteIncome);
 
 router.post('/expense', protect, adminOnly, addExpense);
-router.get('/expenses', protect, adminOnly, getExpenses);
+// router.get('/expenses', protect, adminOnly, getExpenses);
+router.get('/expenses', protect, getExpenses);
+
 router.put('/expense/:id', protect, adminOnly, updateExpense);
 router.delete('/expense/:id', protect, adminOnly, deleteExpense);
 
-router.get('/summary', protect, adminOnly, getFinanceSummary);
-router.get('/download/excel', protect, adminOnly, downloadFinanceExcel);
+// router.get('/summary', protect, adminOnly, getFinanceSummary);
+// router.get('/download/excel', protect, adminOnly, downloadFinanceExcel);
+router.get('/summary', protect,  getFinanceSummary);
+router.get('/download/excel', protect, downloadFinanceExcel);
 
 module.exports = router;
 
